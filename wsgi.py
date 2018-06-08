ba99807cc0aaa5272b902f96c4135627ebb7a3e5
root <root@webserver.jarvistek.home>
2018-06-07 22:02:44 -0400

    think it's working now?

--- wsgi.py
+++ wsgi.py
@@ -5,31 +5,32 @@ application = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@application.route('/hello/')
-@application.route('/hello/<name>')
-def helloname(name='Bob'):
-    return render_template('index.html', name=name)
+@application.route("/hello/")
+@application.route("/hello/<name>")
+def helloname(name="Bob"):
+    return render_template("index.html", name=name)
 
-@application.route('/names/')
+@application.route("/names/")
 def names():
-    table = '''<table style="width:100%">
-    <tr>
-        <th>Firstname</th>
-        <th>Lastname</th> 
-        <th>Age</th>
-    </tr>
-    <tr>
-        <td>Jill</td>
-        <td>Smith</td> 
-        <td>50</td>
-    </tr>
-    <tr>
-        <td>Eve</td>
-        <td>Jackson</td> 
-        <td>94</td>
-    </tr>
-</table>'''
-    return render_template('index.html', name='admin', table=table)
+    table = """        <table style="width:100%">
+            <tr>
+                <th>Firstname</th>
+                <th>Lastname</th> 
+                <th>Age</th>
+            </tr>
+            <tr>
+                <td>Jill</td>
+                <td>Smith</td> 
+                <td>50</td>
+            </tr>
+            <tr>
+                <td>Eve</td>
+                <td>Jackson</td> 
+                <td>94</td>
+            </tr>
+        </table>"""
+    name = "admin"
+    return render_template("index.html", name=name, table=table)
 
 if __name__ == "__main__":
     application.run()
