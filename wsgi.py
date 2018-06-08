81a1dab3a14f3aecaf9562f8a706e912c00a9562
root <root@webserver.jarvistek.home>
2018-06-07 21:49:59 -0400

    getting crazy with the cheesewiz.

--- wsgi.py
+++ wsgi.py
@@ -10,5 +10,26 @@ def hello():
 def helloname(name='Bob'):
     return render_template('index.html', name=name)
 
+@application.route('/names/')
+def names():
+    table = '''<table style="width:100%">
+    <tr>
+        <th>Firstname</th>
+        <th>Lastname</th> 
+        <th>Age</th>
+    </tr>
+    <tr>
+        <td>Jill</td>
+        <td>Smith</td> 
+        <td>50</td>
+    </tr>
+    <tr>
+        <td>Eve</td>
+        <td>Jackson</td> 
+        <td>94</td>
+    </tr>
+</table>'''i
+    return render_template('index.html', name='admin', table=table)
+
 if __name__ == "__main__":
     application.run()
