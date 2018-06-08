d0858fae7c1576547b1a823079bb7dd2072c7682
root <root@webserver.jarvistek.home>
2018-06-07 21:36:34 -0400

    Trying a new example.

---
+++ wsgi.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello World!"
+
+@application.route('/hello/')
+@application.route('/hello/<name>')
+def hello(name='Bob'):
+    return render_template('index.html', name=name)
+
+if __name__ == "__main__":
+    application.run()
